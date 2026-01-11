6d5d81623e1a2e5588b851ff26a0e7a4d3b2a754
Yernoby <ybonreylouiejaugan@gmail.com>
2026-01-11 10:40:14 +0800

    added death in all phases

--- Scripts/attempts.gd
+++ Scripts/attempts.gd
@@ -4,7 +4,7 @@ extends Label
 func _process(_delta: float) -> void:
 	self.text = "Attempt: " + str(GameManager.attempt)
 
-	if GameManager.attempt ==10:
+	if GameManager.attempt ==5:
 		$"../Control/UIs/GameOver".show()
 		GameManager.attempt = 0
 		print(8)
